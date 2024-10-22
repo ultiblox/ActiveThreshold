3ed446f37c8955256e78e5f30cdc49910bf64ae8
Maker Zone <dev@makerzone.com.au>
2024-10-22 19:37:53 +1100

    Moving pin number to variable

--- examples/ActiveThresholdExample/ActiveThresholdExample.ino
+++ examples/ActiveThresholdExample/ActiveThresholdExample.ino
@@ -2,8 +2,10 @@
 
 ActiveThreshold threshold;
 
+int ledPin = 13;
+
 void setup() {
-    pinMode(5, OUTPUT);  // Example pin for LED control
+    pinMode(ledPin, OUTPUT);  // Example pin for LED control
 
     // Initialize the threshold system
     threshold.init()
@@ -18,22 +20,22 @@ void setup() {
 }
 
 void loop() {
-    int sensorValue = analogRead(A0);  // Read analog value from sensor
+    int sensorValue = analogRead(A0);
     Serial.print("Sensor Value: ");
     Serial.println(sensorValue);
 
-    threshold.evaluate(sensorValue);   // Evaluate the sensor value
-    delay(1000);  // Delay between readings
+    threshold.evaluate(sensorValue);
+    delay(1000);
 }
 
 // Callback to handle active state
 void turnOnPin() {
-    digitalWrite(5, HIGH);  // Set pin 13 HIGH (turn on LED)
+    digitalWrite(ledPin, HIGH);
     Serial.println("Active state: ON");
 }
 
 // Callback to handle inactive state
 void turnOffPin() {
-    digitalWrite(5, LOW);  // Set pin 13 LOW (turn off LED)
+    digitalWrite(ledPin, LOW);
     Serial.println("Active state: OFF");
 }
